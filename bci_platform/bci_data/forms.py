3353456f995d39b5ae62f4bdc12605421d0fb9de
allenkang92 <ksw8841@knou.ac.kr>
2024-07-10 13:55:18 +0900

    bootstrap 사용

--- bci_platform/bci_data/forms.py
+++ bci_platform/bci_data/forms.py
@@ -5,8 +5,20 @@ class BCISessionForm(forms.ModelForm):
     class Meta:
         model = BCISession
         fields = ['session_name', 'date_recorded', 'subject_id']
+        widgets = {
+            'session_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'date_recorded': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+            'subject_id': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 class BCIDataForm(forms.ModelForm):
     class Meta:
         model = BCIData
         fields = ['timestamp', 'channel_1', 'channel_2', 'channel_3', 'channel_4']
+        widgets = {
+            'timestamp': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+            'channel_1': forms.NumberInput(attrs={'class': 'form-control'}),
+            'channel_2': forms.NumberInput(attrs={'class': 'form-control'}),
+            'channel_3': forms.NumberInput(attrs={'class': 'form-control'}),
+            'channel_4': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
