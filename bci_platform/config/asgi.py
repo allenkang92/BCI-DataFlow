7d92feb8ed0c45cddb9080696e138f7d9a33149c
allenkang92 <ksw8841@knou.ac.kr>
2024-07-11 17:02:18 +0900

    수정

--- bci_platform/config/asgi.py
+++ bci_platform/config/asgi.py
@@ -12,6 +12,7 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+import bci_data.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
@@ -22,7 +23,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            routing.websocket_urlpatterns
+            bci_data.routing.websocket_urlpatterns
         )
     ),
 })
